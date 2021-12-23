79003bbbdab42dba11c4282722537cba5cbfca03
DakMega <91286468+DakMega@users.noreply.github.com>
2021-12-24 01:08:58 +0400

    Alpha 1.2

--- OOP/Source.cpp
+++ OOP/Source.cpp
@@ -57,28 +57,8 @@ int main() {
 	MaxKuprin maxKuprin;
 	Player firstPlayer;
 	NPC npc;
-	Evil evil("Input control",100,5);
-	/*cout << "Hello, student \nGet rest beside the fire\n";
-	cout << "You are one of the heroes, I guess?\n";
-	cout << "Do you remember your story?\n";
-	int answer;
-	cin >> answer;
-	if (answer == 1) {
-		fin.open(src);
-		if (!fin.is_open()) {
-			cout << "No save file" << endl;
-		}
-		else {
-			Player PLR;
-			
-			while (fin.read((char*)&PLR, sizeof(Player))) {
+	Evil evil("Input control", 100, 5);
 
-			}
-			cout << "Loading succesfully" << endl;
-			fin.close();
-		}
-	}
-	*/
 	cout << "Welcome, dear player. Let's get started!";
 	Sleep(500);
 	cout << "Who are you?\n";
@@ -88,35 +68,35 @@ int main() {
 	NPC* currClass = nullptr;
 	cin >> choice;
 	switch (choice) {
-		case 1: {
-			firstPlayer.create(&student);
-			currClass = &student;
-			break;
-		}
-			  
-		case 2: {
-			firstPlayer.create(&zaochnik);
-			currClass = &zaochnik;
-			break;
-		}
-			  
-		case 3: {
-			firstPlayer.create(&foreignStudent);
-			currClass = &foreignStudent;
-			break;
-		}
-		case 4: {
-			firstPlayer.create(&maxKuprin);
-			currClass = &maxKuprin;
-			break;
-		}
+	case 1: {
+		firstPlayer.create(&student);
+		currClass = &student;
+		break;
+	}
+
+	case 2: {
+		firstPlayer.create(&zaochnik);
+		currClass = &zaochnik;
+		break;
+	}
+
+	case 3: {
+		firstPlayer.create(&foreignStudent);
+		currClass = &foreignStudent;
+		break;
+	}
+	case 4: {
+		firstPlayer.create(&maxKuprin);
+		currClass = &maxKuprin;
+		break;
+	}
 
 
-		
+
 	}
 	//firstPlayer.getInfo(currClass);
 	//evil.getInfo();
-	
+
 	firstPlayer.getInfo(currClass);
 	cout << "What guild are you from?\n 1 - StudClub  2 - ProfCom  3 - ActiveGroup \t";
 	Sleep(1000);
@@ -124,23 +104,23 @@ int main() {
 	cin >> group;
 
 	switch (group) {
-		case 1: {
-			cout << "30% damage increase" << endl;
-			firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 1.3));
-			break;
-		}
-		case 2: {
-			cout << "50% health increase and 20% damage decrease" << endl;
-			firstPlayer.setHealth(currClass, (firstPlayer.getHealth(currClass) * 1.5));
-			firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 0.8));
-			break;
-		}
-		case 3: {
-			cout << "10% health increase and 10% damage increase" << endl;
-			firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 1.1));
-			firstPlayer.setHealth(currClass, (firstPlayer.getHealth(currClass) * 1.1));
-			break;
-		}
+	case 1: {
+		cout << "30% damage increase" << endl;
+		firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 1.3));
+		break;
+	}
+	case 2: {
+		cout << "50% health increase and 20% damage decrease" << endl;
+		firstPlayer.setHealth(currClass, (firstPlayer.getHealth(currClass) * 1.5));
+		firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 0.8));
+		break;
+	}
+	case 3: {
+		cout << "10% health increase and 10% damage increase" << endl;
+		firstPlayer.setDamage(currClass, (firstPlayer.getDamage(currClass) * 1.1));
+		firstPlayer.setHealth(currClass, (firstPlayer.getHealth(currClass) * 1.1));
+		break;
+	}
 
 	}
 	//firstPlayer.healthUpdate();
@@ -161,35 +141,23 @@ int main() {
 		}
 		Sleep(1000);
 	}
-
+	/*Evil evil2("session", 500, 10);
+	while (evil2.getHealth() > 0) {
+		if (choice == 1) {
+			Fight(evil2, firstPlayer, student);
+		}
+		if (choice == 2) {
+			Fight(evil2, firstPlayer, zaochnik);
+		}
+		if (choice == 3) {
+			Fight(evil2, firstPlayer, foreignStudent);
+		}if (choice == 4) {
+			Fight(evil2, firstPlayer, maxKuprin);
+		}
+		Sleep(1000);
+	}*/
 	firstPlayer.lvlUP(currClass,1);
 	firstPlayer.healthUpdate(currClass);
-	/*if (choice == 1) {
-		Fight(evil, firstPlayer, student);
-	}
-	if (choice == 2) {
-		Fight(evil, firstPlayer, zaochnik);
-	}
-	if (choice == 3) {
-		Fight(evil, firstPlayer, foreignStudent);
-	}if (choice == 4) {
-		Fight(evil, firstPlayer, maxKuprin);
-	}*/
-	
-	/*cout << "Save progress?";
-	int save;
-	cin >> save;
-	if (save == 1) {
-		fout.open(src, fstream::app);
-		if (!fout.is_open()) {
-			cout << "Save error"<<endl;
-		}
-		else {
-			cout << "Saved successfully" << endl;
-			fout.write((char*)&firstPlayer,sizeof(Player));
-		}
-		fout.close();
-	}*/
 	firstPlayer.getInfo(currClass);
 	return 0;
 }
